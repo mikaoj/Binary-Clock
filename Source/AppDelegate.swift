fcb5331f50b1f1658028a720cb856064b6ed26ce
Joakim Gyllström <joakim@backslashed.se>
2016-10-16 10:03:58 +0200

    Added launch image Stop screen saver

--- Source/AppDelegate.swift
+++ Source/AppDelegate.swift
@@ -14,7 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Stop screen from turning off
+        UIApplication.shared.isIdleTimerDisabled = true
+        
         return true
     }
 }
